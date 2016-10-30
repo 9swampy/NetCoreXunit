26d97ae866bc0746d70e652c42553c2da3bb551f
9swampy <github@prims.com>
2016-10-30 14:05:03 +0000

    Add second test assembly

---
+++ src/NetCoreXunitB/ModelFixtureB.cs
@@ -0,0 +1,30 @@
+﻿using NetCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace NetCoreXunitB
+{
+  public class ModelFixtureB
+  {
+    [Fact]
+    public void GivenInvertingLogicWhenInvertCalledThenShouldInvertResponse()
+    {
+      var sut = new Model();
+      bool expected = (new Random().Next() % 2) == 1;
+
+      sut.ReplyInverted(!expected).Should().Be(expected);
+    }
+
+    [Fact]
+    public void GivenDirectLogicWhenReplySameCalledThenShouldReplySame()
+    {
+      var sut = new Model();
+      bool expected = (new Random().Next() % 2) == 1;
+      sut.ReplySame(expected).Should().Be(expected);
+    }
+  }
+}
---
+++ src/NetCoreXunitB/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetCoreXunitB")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f7ae33df-b99e-46fe-9e70-3ecdaabf9256")]
